b8fa1686cf6c1e5b8eff2dc81394b161d1a2a3ae
Ghidyon <gideonakunana@gmail.com>
2020-09-18 12:17:27 +0100

    third commit

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const computeMean = arr => {
+    let sumOfItems = arr.reduce((a, b) => a + b);
+    return sumOfItems / arr.length;
+}
+
+const computeMedian = arr => {
+    let arrIndex = arr.length / 2;
+    if (arr.length % 2 === 0) {
+        return (arr[arrIndex] + arr[arrIndex - 1]) / 2;
+    } else {
+        return arr[Math.floor(arrIndex)];
+    }
+}
+
+const computeMode = arr => {
+    //Using the formular gotten from the empirical relationship between mean, median and mode
+    return computeMean(arr) - 3 * (computeMean(arr) - computeMedian(arr));
+}
+
 const compute = arr => {
     const newArray = [];
 
@@ -13,14 +32,19 @@ const compute = arr => {
     arr.forEach(removeAlphanumerals);
 
     const filteredArray = newArray.sort((a, b) => b - a);
-    return filteredArray;
+
+    const mean = computeMean(filteredArray);
+
+    const median = computeMedian(filteredArray);
+
+    const mode = computeMode(filteredArray);
+
+    return {
+        filteredArray,
+        mean,
+        median,
+        mode
+    }
 }
 
 console.log(compute([1, "3ab", 2, "ef4", "g3h", 5]));
-
-
-
-
-/* const computeMean = arr => {
-    return mean
-} */
